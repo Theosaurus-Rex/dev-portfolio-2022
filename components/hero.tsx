a2aa4fefd63a09565ca453032e04806ea03beeef
Theo Harris <theosaurus13@gmail.com>
2022-05-05 08:47:50 +1000

    Add theme color extension, style hero text

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 export const Hero = () => {
     return (
       <>
-        <p>Hi, I&apos;m Theo Harris</p>
-        <h1>Web Developer and Visual Artist from Sydney, Australia.</h1>
-        <p>
+        <p className="text-purple-heart-500 font-medium text-sm">Hi, I&apos;m Theo Harris</p>
+        <h1 className="text-gray-900 font-semibold text-6xl font-display">Web Developer and Visual Artist from Sydney, Australia.</h1>
+        <p className="text-xl text-gray-500">
           Passionate about working in the intersection of visual communication
           and the web. I believe in creating digital experiences that are both
           delightful and accessible.
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-heart": {
+          DEFAULT: "#6941C6",
+          50: "#D8CEF0",
+          100: "#CCBFEC",
+          200: "#B39FE2",
+          300: "#9B80D9",
+          400: "#8260CF",
+          500: "#6941C6",
+          600: "#51309F",
+          700: "#3B2374",
+          800: "#251649",
+          900: "#0F091E",
+        },
+      },
+      fontFamily: {
+        display: ["Raleway", "sans-serif"],
+        body: ["Inter", "sans-serif"]
+      },
+    },
   },
   plugins: [],
-}
+};
