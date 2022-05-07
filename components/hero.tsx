8e004466d37459d66af7030fede39a4bf4c4112b
Theo Harris <theosaurus13@gmail.com>
2022-05-08 08:14:55 +1000

    Finish major styling for Hero Adjust primary purple hue

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,15 +1,24 @@
 import Link from "next/link";
 export const Hero = () => {
-    return (
-      <>
-        <p className="text-purple-heart-500 font-medium text-sm font-inter">Hi, I&apos;m Theo Harris</p>
-        <h1 className="text-gray-900 font-semibold text-6xl font-raleway">Web Developer and Visual Artist from Sydney, Australia.</h1>
-        <p className="text-xl text-gray-500 font-inter">
-          Passionate about working in the intersection of visual communication
-          and the web. I believe in creating digital experiences that are both
-          delightful and accessible.
-        </p>
-        <Link href="" className="font-inter">See My Work</Link>
-      </>
-    );
-  }
+  return (
+    <div className="flex flex-col space-y-4">
+      <p className="text-medium-purple-700 text-sm font-inter border-4 border-slate-50 w-fit rounded-xl px-2">
+        Hi, I&apos;m Theo Harris
+      </p>
+      <h1 className="text-slate-900 font-semibold text-6xl font-raleway">
+        Web Developer and Visual Artist from Sydney, Australia.
+      </h1>
+      <p className="text-xl text-slate-500 font-light font-inter">
+        Passionate about working in the intersection of visual communication and
+        the web. I believe in creating digital experiences that are both
+        delightful and accessible.
+      </p>
+      <a
+        href=""
+        className="font-inter bg-medium-purple-600 hover:bg-medium-purple-700 text-white px-4 py-2 rounded-md w-fit"
+      >
+        See My Work
+      </a>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "purple-heart": {
-          DEFAULT: "#6941C6",
-          50: "#D8CEF0",
-          100: "#CCBFEC",
-          200: "#B39FE2",
-          300: "#9B80D9",
-          400: "#8260CF",
-          500: "#6941C6",
-          600: "#51309F",
-          700: "#3B2374",
-          800: "#251649",
-          900: "#0F091E",
+        "medium-purple": {
+          DEFAULT: "#916CE6",
+          50: "#FFFFFF",
+          100: "#F9F7FE",
+          200: "#DFD5F8",
+          300: "#C5B2F2",
+          400: "#AB8FEC",
+          500: "#916CE6",
+          600: "#6D3CDE",
+          700: "#5121C1",
+          800: "#3D1991",
+          900: "#291061",
         },
       },
       fontFamily: {
         raleway: ["Raleway", "sans-serif"],
-        inter: ["Inter", "sans-serif"]
+        inter: ["Inter", "sans-serif"],
       },
     },
   },
