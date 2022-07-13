4e168bca853d124e77d3bd07c12686a44f6d1d9c
Theosaurus-Rex <theosaurus13@gmail.com>
2022-07-13 17:06:02 +1000

    Add in 3D avatar

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
-import heroPlaceholder from "/public/img/placeholder_hero.png";
+import heroAvatar from "/public/img/avatar.png";
 export const Hero = () => {
   return (
-    <div className="py-20 lg:grid lg:grid-cols-2 w-full items-center content-center">
+    <div className="py-20 lg:grid lg:grid-cols-[1.4fr_1fr] w-full items-center content-center">
       <div className="px-20 col-start-1 col-span-1 flex flex-col space-y-4">
         <p className="text-medium-purple-600 text-sm font-inter border-4 border-slate-50 w-fit rounded-xl px-2">
           Hi, I&apos;m Theo Harris
@@ -25,8 +25,8 @@ export const Hero = () => {
       </div>
       <div className="hidden col-start-2 col-span-1 w-full drop-shadow-2xl md:block">
         <Image
-          src={heroPlaceholder}
-          alt={"Cartoon character with a VR headset and controllers"}
+          src={heroAvatar}
+          alt={"A 3D cartoon of Theo"}
           layout="responsive"
         />
       </div>
